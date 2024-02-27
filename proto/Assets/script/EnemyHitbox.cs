0b1a7a42fc4e4fdcdd33dfa08199b0e58a937958
CleoInStripes <cleoinstripes@gmail.com>
2024-02-26 18:26:30 -0600

    Collisions

--- proto/Assets/script/EnemyHitbox.cs
+++ proto/Assets/script/EnemyHitbox.cs
@@ -7,21 +7,23 @@ public class EnemyHitbox1 : MonoBehaviour
 
 
     public float fDamage = 20;
-    private Vector3 v3Knockback = new Vector3(0, 5, 15);
+    private Vector3 v3Knockback = new Vector3(5, 0, 0);
 
-    private LayerMask layerMask;
+    public LayerMask layerMask;
 
     public LayerMask LayerMask { get { return layerMask; } set => layerMask = value; }
 
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-    
-        if (LayerMask == (LayerMask | (1 << other.transform.gameObject.layer))) {
+        Debug.Log("Trigger");
+        if (layerMask == (LayerMask | (1 << other.transform.gameObject.layer))) {
+           
         
             PlayerHurtbox h = other.GetComponent<PlayerHurtbox>();
             if (h != null) {
             
                 h.playerhealth.playerhealth -= fDamage;
+                Debug.Log(h.playerhealth.playerhealth);
                 h.cc.playerDirection = v3Knockback;
                 Destroy(this.gameObject);
              }
