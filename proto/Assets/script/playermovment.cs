86d6a37fdbe3af672ac3fcb6157301c0f5bf4ca6
CleoInStripes <cleoinstripes@gmail.com>
2024-03-04 11:52:03 -0600

    Movement Animation
    Still WIP

--- proto/Assets/script/playermovment.cs
+++ proto/Assets/script/playermovment.cs
@@ -7,11 +7,13 @@ public class playermovement  : MonoBehaviour
     public float playerspeed;
     private  Rigidbody2D rb;
     public Vector2 playerDirection;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +27,8 @@ public class playermovement  : MonoBehaviour
     void FixedUpdate()
     {
         rb.velocity = new Vector2(playerDirection.x * playerspeed, playerDirection.y * playerspeed);
+
+        anim.SetFloat("xSpeed", rb.velocity.x);
+        anim.SetFloat("ySpeed", rb.velocity.y);
     }
 }
